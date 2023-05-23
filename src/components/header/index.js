80bced825daa2ed187380ea20e1e244cf0cd61b7
serenity smith <serenitysmith24@gmail.com>
2023-05-22 23:30:36 -0400

    added  con

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,12 +6,14 @@ function Header() {
     <header>
       <nav className="navbar">
 <img src={logo} alt="logo" className="reactlogo"></img>
+<h3>ReactFacts</h3>
+            <h6>React Course - Project 1</h6>
 
-        <ul className="list">
+        {/* <ul className="list">
           <li>Price</li>
           <li>About</li>
           <li>Contact</li>
-        </ul>
+        </ul> */}
       </nav>
     </header>
   );
--- src/components/maincontent/index.js
+++ src/components/maincontent/index.js
@@ -1,16 +1,18 @@
 import React from "react";
-
+import logo from "./greyyy.png"
 export default function MainContent() {
     return (
         // div  parent, everything inside children
         <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol className="orderedli">
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
+            <h1 className="facts">Fun Facts About React</h1>
+            <ul className="orderedli">
+                <li>It's a popular library</li>
                 <li>I'm more likely to get a job as a developer
                 if I know React</li>
-            </ol>
+                <li>Its maintianed by Facebook</li>
+                <li>Powers thousands of apps </li>
+            </ul>
+            <img src={logo} alt="Logo" className="greylogo"></img>
         </div>
     )
 }
